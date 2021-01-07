f21bc080ecf5dcc885e446567f8545726634c839
GokulnathP <4gokul2000@gmail.com>
2021-01-07 07:47:01 +0530

    Refactor. private method to move a ball and ball have it's speed

--- src/main/java/com/gokulnathp/Ball.java
+++ src/main/java/com/gokulnathp/Ball.java
@@ -1,24 +1,24 @@
 package com.gokulnathp;
 
 public class Ball {
-    private final int ballNumber;
+    private final int speed;
     private int xPosition;
     private final int yPosition;
     private final int diameter;
 
-    public Ball(int ballNumber, int xPosition, int yPosition, int diameter) {
-        this.ballNumber = ballNumber;
+    public Ball(int speed, int xPosition, int yPosition, int diameter) {
+        this.speed = speed;
         this.xPosition = xPosition;
         this.yPosition = yPosition;
         this.diameter = diameter;
     }
 
-    public void setxPosition() {
-        xPosition += ballNumber;
+    private void moveBall() {
+        xPosition += speed;
     }
 
     public void draw(MainProcessing sketch) {
         sketch.ellipse(xPosition, yPosition, diameter, diameter);
-        setxPosition();
+        moveBall();
     }
 }
